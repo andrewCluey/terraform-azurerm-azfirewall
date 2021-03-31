0abc5faebe4a4ee223c3a986c3012aab34815530
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-03-31 23:26:20 +0100

    updated examples directory

--- examples/main.tf
+++ examples/main.tf
@@ -20,3 +20,59 @@ module "azfirewall" {
     DeployedBy = "Terraform"
   }
 }
+
+module "project_out_allow_net_rule" {
+  source  = "andrewCluey/azurefirewall-net-rule-collection/azurerm"
+  version = "1.2.0"
+
+  azure_fw_name           = "CORE-FW"
+  fw_resource_group_name  = "CORE-FW-RG"
+  rule_collection_name    = "dev_project_allow_rules"
+  rule_priority           = "300"
+  rule_action             = "Allow"
+  
+  rule = [
+    {
+      name                  = "ALLOW_https_out"
+      source_addresses      = ["10.0.0.0/8"]
+      destination_ports     = ["443"]
+      destination_addresses = ["0.0.0.0/0"]
+      protocols             = ["TCP"]
+    },
+    {
+      name                  = "ALLOW_sftp_out"
+      source_addresses      = ["10.0.0.0/8"]
+      destination_ports     = ["22"]
+      destination_addresses = ["8.8.8.8"]
+      protocols             = ["TCP"]
+    }
+  ]
+}
+
+module "project_out_deny_net_rule" {
+  source  = "andrewCluey/azurefirewall-net-rule-collection/azurerm"
+  version = "1.2.0"
+
+  azure_fw_name           = "CORE-FW"
+  fw_resource_group_name  = "CORE-FW-RG"
+  rule_collection_name    = "dev_project_deny_rules"
+  rule_priority           = "310"
+  rule_action             = "Deny"
+  
+  rule = [
+    {
+      name                  = "deny_http_out"
+      source_addresses      = ["10.0.0.0/8"]
+      destination_ports     = ["80"]
+      destination_addresses = ["0.0.0.0/0"]
+      protocols             = ["TCP"]
+    },
+    {
+      name                  = "deny_ftp_out"
+      source_addresses      = ["10.0.0.0/8"]
+      destination_ports     = ["21x"]
+      destination_addresses = ["8.8.8.8"]
+      protocols             = ["TCP"]
+    }
+  ]
+}
