c733fcc67be8dcc8e6d0bb603c954eb24b2c45d2
ASC IT Solutions <31044@im-firm.com>
2021-04-01 16:10:04 +0100

    added new deployment options
    New deployment options to:
     - Deploy additional Public IP addresses to the Firewall, using a dynamic configuration block (for_each).
     - Option to attach a Firewall Policy
     -  Thorough example showing the deployment of the module.

--- examples/simple_deployment/main.tf
+++ examples/simple_deployment/main.tf
@@ -2,24 +2,63 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  location    = "UK South"
+  module_tag  = {"module" = basename(abspath(path.module))}
+  env_tag     = {"environment" = "non-Prod"}
+  any_tags     = {"tag1" = "tag1Value", "tag2" = "tag2Value", "location" = local.location, "projectCode" = "PR12345"}
+  tags        = merge(local.module_tag, local.env_tag, local.any_tags)
+}
+
+# Lookup Network for Firewall
 data "azurerm_subnet" "azure_firewall" {
   name                 = "AzureFirewallSubnet"
   virtual_network_name = "core-vnet"
   resource_group_name  = "network"
 }
 
-module "azfirewall" {
-  source  = "andrewCluey/azfirewall/azurerm"
-  version = "1.3.2"
-  
-  resource_group_name = "sazfw"
-  subnet_id           = data.azurerm_subnet.azure_firewall.id
-  environment         = "non-prod"
+# Create additional Public IP - No1
+resource "azurerm_public_ip" "fw_additional_ip01" {
+  name                = "fw-pip01"
+  resource_group_name = "network"
+  location            = local.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.tags
+}
 
-    tags = {
-    DeployedBy = "Terraform"
-  }
+# Create additional Public IP - No2
+resource "azurerm_public_ip" "fw_additional_ip02" {
+  name                = "fw-pip02"
+  resource_group_name = "network"
+  location            = local.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.tags
 }
 
 
-# "git::ssh://git@ssh.dev.azure.com/v3/AzDoOrgName/projectName/terraform-azurerm-ModuleName"
+# Deploy new Azure Firewall from the Module.
+module "azfirewall" {
+  source = "../../"
+  #  source  = "andrewCluey/azfirewall/azurerm"
+  #  version = "2.0.0"
+
+  resource_group_name = "network"
+  location            = local.location
+  subnet_id           = data.azurerm_subnet.azure_firewall.id
+  environment         = "non-prod"
+
+  additional_public_ips = [
+    {
+      name                 = azurerm_public_ip.fw_additional_ip01.name
+      public_ip_address_id = azurerm_public_ip.fw_additional_ip01.id
+    },
+    {
+      name                 = azurerm_public_ip.fw_additional_ip02.name
+      public_ip_address_id = azurerm_public_ip.fw_additional_ip02.id
+    }
+  ]
+
+  tags = local.tags
+}
